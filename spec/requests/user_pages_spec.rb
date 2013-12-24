7b2db6132e06f2551080e01a5e35e73a963e5ad8
mcakavithas <mcakavithas@gmail.com>
2013-12-24 13:09:43 +0530

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,6 +36,14 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+       describe "after saving the user" do
+         before { click_button submit }
+        it { should have_link('Sign out') }
+              # describe "after saving the user" do
+        #it { should have_link('Sign out') }
+      #end
+      end
     end
   end
 end
